b67cc5f2dabd97c95879a04b02dd854d72c59dc6
JackWilb <jackwilburn@tutanota.com>
2022-07-01 10:40:20 -0600

    Add pinia and remove vuex

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,14 @@
 import { createApp } from 'vue';
+import { createPinia } from 'pinia';
+import ElementPlus from 'element-plus';
 import App from './App.vue';
-import store from './store';
-import ElementPlus from 'element-plus'
-import 'element-plus/dist/index.css'
+import 'element-plus/dist/index.css';
 
-createApp(App).use(store).use(ElementPlus).mount('#app');
+const pinia = createPinia();
+const app = createApp(App);
+
+app
+  .use(pinia)
+  .use(ElementPlus);
+
+app.mount('#app');
