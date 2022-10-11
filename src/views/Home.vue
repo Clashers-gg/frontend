18121cb6ea1821426281886010a40e9e399c6c03
polinaLyubavina <polinalyub@tutanota.com>
2022-10-11 15:13:39 -0600

    add skeleton UI for a Home page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,20 @@
 <template>
   <div>
-    <h1>Home</h1>
+    <h1>Welcome to Clashers!</h1>
   </div>
+
+  <el-button type="login">Login</el-button>
+
+  <el-button type="register">Register</el-button>
+
+  <el-button type="primary" :icon="Search">Search</el-button>
+
+  <el-button type="contact">Contact Us</el-button>
+
 </template>
 
 <script setup lang="ts">
+
+  import {Search} from '@element-plus/icons-vue'
 
 </script>
