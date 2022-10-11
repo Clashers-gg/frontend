a3d32b2a7d114ab0b61a9ce8961577e220039255
polinaLyubavina <polinalyub@tutanota.com>
2022-10-11 15:28:43 -0600

    add basic skeleton for a Dashboard page

---
+++ src/views/Dashboard.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <h1>Welcome to Your Clash!</h1>
+    </div>
+
+    <el-button type="profile">Profile</el-button>
+   
+    <el-button type="friends">Friends</el-button>
+   
+    <el-button type="clash">Clash</el-button>
+   
+    <el-button type="rankedduo">Ranked Duo</el-button>
+   
+    <el-button type="contact">Contact Us</el-button>
+
+    <el-button type="notifications">Notifications</el-button>
+
+</template>
+
+<script setup lang="ts">
+</script>
