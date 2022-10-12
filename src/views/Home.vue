51f934f66c3daf4a0eee2551649464c311439235
polinaLyubavina <polinalyub@tutanota.com>
2022-10-12 11:56:27 -0600

    remove type from buttons

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,13 +3,13 @@
     <h1>Welcome to Clashers!</h1>
   </div>
 
-  <el-button type="login">Login</el-button>
+  <el-button>Login</el-button>
 
-  <el-button type="register">Register</el-button>
+  <el-button>Register</el-button>
 
-  <el-button type="primary" :icon="Search">Search</el-button>
+  <el-button :icon="Search">Search</el-button>
 
-  <el-button type="contact">Contact Us</el-button>
+  <el-button>Contact Us</el-button>
 
 </template>
 
