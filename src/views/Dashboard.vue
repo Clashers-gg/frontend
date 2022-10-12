468804ac1defa63b878e97eb67dd9ebc9d7e1d49
polinaLyubavina <polinalyub@tutanota.com>
2022-10-12 11:52:39 -0600

    remove type from buttons

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -3,17 +3,17 @@
         <h1>Welcome to Your Clash!</h1>
     </div>
 
-    <el-button type="profile">Profile</el-button>
+    <el-button>Profile</el-button>
    
-    <el-button type="friends">Friends</el-button>
+    <el-button>Friends</el-button>
    
-    <el-button type="clash">Clash</el-button>
+    <el-button>Clash</el-button>
    
-    <el-button type="rankedduo">Ranked Duo</el-button>
+    <el-button>Ranked Duo</el-button>
    
-    <el-button type="contact">Contact Us</el-button>
+    <el-button>Contact Us</el-button>
 
-    <el-button type="notifications">Notifications</el-button>
+    <el-button>Notifications</el-button>
 
 </template>
 
