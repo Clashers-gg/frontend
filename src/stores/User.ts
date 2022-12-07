3bf7b5a1a29897176d4328f5410b87e4b1407c14
polinaLyubavina <polinalyub@tutanota.com>
2022-12-06 18:23:59 -0700

    Fix dashboard to use the user store

--- src/stores/User.ts
+++ src/stores/User.ts
@@ -1,7 +1,7 @@
 /* eslint-disable import/prefer-default-export */
 import Cookies from 'js-cookie';
 import { defineStore } from 'pinia';
-import { computed, ref } from 'vue';
+import { computed, ref, watch } from 'vue';
 
 export const useUser = defineStore('user', () => {
   const userRiotID = ref('');
@@ -18,7 +18,6 @@ export const useUser = defineStore('user', () => {
       },
     })
 
-    console.log(whoamiResponse)
     if (whoamiResponse.status === 200 && !whoamiResponse.redirected) {
       userRiotID.value = await whoamiResponse.text();
     } else {
@@ -26,5 +25,23 @@ export const useUser = defineStore('user', () => {
     }
   }
 
-  return { checkUserLoggedIn, userLoggedIn, userRiotID, }
+  const userInfo = ref({ name: '', summonerLevel: '', profileIconId: ''});
+
+  async function getUserInfo() {
+    const csrf = Cookies.get('csrftoken') || '';
+    const response = await fetch(`http://localhost:8000/get_player_info`, {
+        method: 'GET',
+        credentials: 'include', 
+        mode: 'cors',
+        headers: {
+            'X-CSRFToken': csrf
+        },
+    })
+
+    userInfo.value = await response.json();
+  }
+
+  watch(userRiotID, () => getUserInfo());
+
+  return { checkUserLoggedIn, userInfo, userLoggedIn, userRiotID, }
 });
--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,46 +1,22 @@
 <template>
     <div>
-        <h1>Welcome to Your Clash, {{player.name}}!</h1>
+        <h1>Welcome to Your Clash, {{ userRiotID }}!</h1>
     </div>
 
     <div style="margin: 20px 0" />
 
-    <div v-if="Object.keys(player).length > 0">
-        <img width="100" height="100" :src="`https://ddragon.leagueoflegends.com/cdn/12.22.1/img/profileicon/${player.profileIconId}.png`" >
-        <p>Summoner Level: {{player.summonerLevel}} </p>
+    <div v-if="userRiotID !== ''">
+        <img width="100" height="100" :src="`https://ddragon.leagueoflegends.com/cdn/12.22.1/img/profileicon/${userInfo.profileIconId}.png`" >
+        <p>Summoner Level: {{userInfo.summonerLevel}} </p>
     </div>
 
 </template>
 
 <script setup lang="ts">
-    import Cookies from 'js-cookie';
-    import {ref} from 'vue'
+import { computed } from 'vue';
+import { useUser } from '../stores/user';
 
-    const player = ref({})
-
-            fetch(`http://localhost:8000/get_player_info`, {
-
-                method: 'GET',
-
-                credentials: 'include', 
-
-                mode: 'cors',
-
-                headers: {
-                    'X-CSRFToken': Cookies.get('csrftoken')
-                },
-
-            } )
-
-            .then((response) => response.json() )
-            .then((data) => {
-                console.log('Success:', data);
-                //do something useful with data here (store data, display, etc)
-                player.value = data;
-            } )
-
-            .catch( (error) => {
-                    console.error('Error:', error);
-                }
-            );
+const store = useUser();
+const userRiotID = computed(() => store.userRiotID);
+const userInfo = computed(() => store.userInfo);
 </script>
