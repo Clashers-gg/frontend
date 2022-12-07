9ffce372291fa01e0b1168af7a9540734ca51b9a
polinaLyubavina <polinalyub@tutanota.com>
2022-12-06 18:11:23 -0700

    Create a central user store with the logged in user information

--- src/stores/User.ts
+++ src/stores/User.ts
@@ -1,18 +1,30 @@
 /* eslint-disable import/prefer-default-export */
+import Cookies from 'js-cookie';
 import { defineStore } from 'pinia';
+import { computed, ref } from 'vue';
 
-export const useUser = defineStore('User', {
-  state() {
-    return {
+export const useUser = defineStore('user', () => {
+  const userRiotID = ref('');
+  const userLoggedIn = computed(() => userRiotID.value !== '');
 
-    };
-  },
+  async function checkUserLoggedIn() {
+    const csrf = Cookies.get('csrftoken') || '';
+    const whoamiResponse = await fetch('http://localhost:8000/whoami', {
+      method: 'GET',
+      credentials: 'include', 
+      mode: 'cors',
+      headers: {
+        'X-CSRFToken': csrf
+      },
+    })
 
-  getters: {
+    console.log(whoamiResponse)
+    if (whoamiResponse.status === 200 && !whoamiResponse.redirected) {
+      userRiotID.value = await whoamiResponse.text();
+    } else {
+      userRiotID.value = ''
+    }
+  }
 
-  },
-
-  actions: {
-
-  },
+  return { checkUserLoggedIn, userLoggedIn, userRiotID, }
 });
